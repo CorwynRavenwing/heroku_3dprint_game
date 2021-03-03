bfb6e6bc1634658485bda158b4b0cb0f8cee231a
Warren Harmon <oakenraven@juno.com>
2021-03-03 14:58:52 -0500

    a few changes to leftbar and body

--- web/index.php
+++ web/index.php
@@ -6,17 +6,19 @@
 <body>
 <div class="outer">
 	<div class="leftbar">
-		Left Bar
+		<div class="data">
+			Money:&nbsp$<p id="data_money">0</p>
+		</div>
 	</div>
 	<div class="main">
 <?php
 $Blocks = 20;
 for($x = 0; $x < $Blocks; $x++) {
-	echo "
-	<div class='block' id='block_{$x}'>
-		(block {$x})
+	?>
+	<div class="block" id="block_<?=$x?>}">
+		(block <?=$x?>)
 	</div>
-	";
+	<?php
 } # next Blocks
 ?>
 	</div>
