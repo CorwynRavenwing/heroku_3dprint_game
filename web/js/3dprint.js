39f2127a3bd1da569882671b57407bb7acf0d9fd
Warren Harmon <oakenraven@juno.com>
2021-03-08 15:42:07 -0500

    load/save to localStorage

--- web/js/3dprint.js
+++ web/js/3dprint.js
@@ -6,7 +6,38 @@ $(document).ready(function() {
 		return;
 	}
 
-	$("#data_filament").html(10);
-	$("#data_kits").html(1);
+	var data = {};
+
+	var load_data = function () {
+		console.log('called function load_data');
+		data['filament'] = localStorage.getItem('filament');
+	}
+
+	var save_data = function () {
+		console.log('called function save_data');
+		localStorage.setItem('filament', data['filament']);
+	}
+
+	var update_screen = function () {
+		console.log('called function update_screen');
+		$("#data_filament").html(data['filament']);
+		$("#data_kits").html(data['kits']);
+	}
+
+	var begin = function () {
+		console.log('called function begin');
+		data['filament'] = 10;
+		data['kits'] = 1;
+		data['version'] = 1;
+	}
+
+	load_data();
+
+	if (!data.version) {
+		begin();
+		save_data();
+	}
+
+	update_screen();
 
 });
