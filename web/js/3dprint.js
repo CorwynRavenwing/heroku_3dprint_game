41c0c8dc6ca6a23642591cc5abddc8c6997b5440
Warren Harmon <oakenraven@juno.com>
2021-03-08 17:31:41 -0500

    display different messages during load/save

--- web/js/3dprint.js
+++ web/js/3dprint.js
@@ -1,12 +1,13 @@
 /* 3dprint_game/js/3dprint.js */
 
+var data = {};
+
 $(document).ready(function() {
 	if (typeof(Storage) === "undefined") {
 		$(".main").html("Sorry! No Web Storage support. You need a more recent browser.");
 		return;
 	}
 
-	var data = {};
 	var data_items = [
 		'filament',
 		'kits',
@@ -17,24 +18,24 @@ $(document).ready(function() {
 	var load_data = function () {
 		console.log('called function load_data');
 		data_items.forEach(function(item, index, array) {
-			console.log(item, index);
 			data[index] = localStorage.getItem(index);
+			console.log(index, data[index]);
 		});
 	}
 
 	var save_data = function () {
 		console.log('called function save_data');
 		data_items.forEach(function(item, index, array) {
-			console.log(item, index);
 			localStorage.setItem(index, data[index]);
+			console.log(index, data[index]);
 		});
 	}
 
 	var update_screen = function () {
 		console.log('called function update_screen');
 		data_items.forEach(function(item, index, array) {
-			console.log(item, index);
 			$("#data_"+index).html(data[index]);
+			console.log(index, data[index]);
 		});
 	}
 
