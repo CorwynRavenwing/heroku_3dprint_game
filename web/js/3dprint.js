b00146ecc2d62ba398c15fbc13bc754ccd93b036
Warren Harmon <oakenraven@juno.com>
2021-03-07 21:09:06 -0500

    reverse polarity of No Web Storage test

--- web/js/3dprint.js
+++ web/js/3dprint.js
@@ -1,11 +1,11 @@
 /* 3dprint_game/js/3dprint.js */
 
 $(document).ready(function() {
-	if (typeof(Storage) !== "undefined") {
+	if (typeof(Storage) === "undefined") {
 		$(".main").html("Sorry! No Web Storage support. You need a more recent browser.");
 		return;
 	}
 
-	$("#data_money") = 1000;
+	$("#data_filament") = 10;
 
 });
