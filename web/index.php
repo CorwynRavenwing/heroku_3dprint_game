8ed3d2452eae02607668b437a7d839950b00ff7a
Warren Harmon <oakenraven@juno.com>
2021-03-13 14:02:21 -0500

    more

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 <link rel="manifest" href="/site.webmanifest">
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/3dprint.css">
 <script type="text/javascript" src="public/static/jquery/jquery.js"></script>
 <script type="text/javascript" src="js/3dprint.js"></script>
 <title>3d Print Game</title>
@@ -15,7 +15,7 @@
 	<div class="leftbar">
 		<!-- div.data, containing Label: div#data_x, added here automatically -->
 	</div>
-	<div class="main">
+	<div class="blocks">
 		<noscript>Sorry, you need JavaScript to run this game.</noscript>
 		<!-- div.block added here automatically -->
 	</div>
