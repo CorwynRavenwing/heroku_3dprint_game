f556273e7f7aab4a62264a0ab57911cec1c8ebb5
Warren Harmon <oakenraven@juno.com>
2021-03-31 17:58:34 -0400

    finish moving chooser functions to JS file

--- web/js/3dprint.js
+++ web/js/3dprint.js
@@ -24,46 +24,6 @@ var announce = function (announcement) {
 		.html(announcement);
 }
 
-var chooser = function (headline, choices, current_value, callback) {
-	$('.chooser').show();
-	var choose_head = $('.chooser .header')
-		.text(headline);
-	var choose_body = $('.chooser .body');
-	var selector = $('<select>')
-		.attr("id", "chooser_selector")
-		.appendTo(choose_body);
-
-	Object.keys(choices).forEach(function(optionText){
-		var optionValue = choices[optionText];
-		var opt = $('<option>')
-			.val(optionValue)
-			.text(optionText)
-			.appendTo(selector);
-		if (optionValue == current_value) {
-			opt.attr("selected", "selected");
-		}
-	});
-
-	var hide_chooser = function () {
-		choose_head.text("[headline]");
-		selector.remove();
-		$('.chooser').hide();
-	}
-
-	$('.chooser #ok').click(function(){
-		var chosen_option = selector.find("option:selected");
-		var text  = chosen_option.text();
-		var value = chosen_option.val();
-
-		callback(value, text);
-		hide_chooser();
-	});
-
-	$('.chooser #cancel').click(function(){
-		hide_chooser();
-	});
-}
-
 var clear_all_data = function () {
 	D.clearAll();
 	load_data();
