458f0268ce918677a400b846ed7af4334dfeefe2
Warren Harmon <oakenraven@juno.com>
2021-04-07 22:23:16 -0400

    missing "this"

--- web/js/3d-block.js
+++ web/js/3d-block.js
@@ -321,7 +321,7 @@ class Block {
 	}
 
 	add_section(subtype, label) {
-		add_section_NEW(subtype_label);		// call other function too
+		this.add_section_NEW(subtype_label);		// call other function too
 
 		var self=this;
 		var outer = $('<div>')
